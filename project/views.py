6a59c811cc1aa998d73212e791d8fc937c687587
Your Name <you@example.com>
2023-04-10 21:06:55 +0800

    ipgeo

--- project/views.py
+++ project/views.py
@@ -2,7 +2,9 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth.decorators import login_required  
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout   
+import json 
+import requests 
 
 @login_required(login_url='login') 
 def home(request):
@@ -16,7 +18,7 @@ def register(request):
       pass2=request.POST.get('password2')  
 
       if pass1!=pass2: 
-        return HttpResponse("Нууц үг зөрүүтэй байна!!!") 
+        messages.error("Нууц үг таарахгүй байна!!!") 
       else: 
         my_user=User.objects.create_user(uname,email,pass1) 
         my_user.save() 
@@ -41,6 +43,17 @@ def Logout(request):
   return redirect('login') 
 
 @login_required(login_url='login') 
-def Ipgeo(request):  
-   return render(request, 'ipgeo.html') 
+def Ipgeo(request):   
+  ip = requests.get('https://api.ipify.org?format=json')   
+  ip_data = json.loads(ip.text)   
+  res = requests.get('http://ip-api.com/json/'+ip_data["ip"]) 
+  location_data_one = res.text 
+  location_data = json.loads(location_data_one)
+  if request.POST.get('Theip'):   
+      ip_data = str(request.POST.get('Theip'))   
+      res = requests.get('http://ip-api.com/json/'+ip_data) 
+      location_data_one = res.text 
+      location_data = json.loads(location_data_one)
+  return render(request, 'ipgeo.html', { 'data' : location_data })
+
 
